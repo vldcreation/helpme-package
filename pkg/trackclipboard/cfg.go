180a7352413adfc35a9bbe706985cbbefdcf92c9
Vicktor Desrony <vldcreation21@gmail.com>
2025-03-22 15:25:01 +0700

    refactor(trackclipboard): restructure Config struct for better configuration handling
    Refactor the Config struct to include an APPConfig struct and add YAML, environment, and mapstructure tags for improved configuration management. This change enhances maintainability and aligns with a more structured configuration approach.

--- pkg/trackclipboard/cfg.go
+++ pkg/trackclipboard/cfg.go
@@ -3,18 +3,21 @@ package trackclipboard
 import "time"
 
 type Config struct {
-	Channel  string
-	Idle     time.Duration
-	File     *FileConfig
-	Telegram *TelegramConfig
+	App      *APPConfig      `yaml:"app" env:"APP" mapstructure:"app"`
+	File     *FileConfig     `yaml:"file" env:"FILE" mapstructure:"file"`
+	Telegram *TelegramConfig `yaml:"telegram" env:"TELEGRAM" mapstructure:"telegram"`
 }
 
+type APPConfig struct {
+	Channel string        `yaml:"channel" env:"APP_CHANNEL" mapstructure:"app_channel"`
+	Idle    time.Duration `yaml:"idle" env:"APP_IDLE" mapstructure:"app_idle"`
+}
 type FileConfig struct {
-	Path string
-	Name string
+	Path string `yaml:"path" env:"FILE_PATH" mapstructure:"file_path"`
+	Name string `yaml:"name" env:"FILE_NAME" mapstructure:"file_name"`
 }
 
 type TelegramConfig struct {
-	Token  string
-	ChatID string
+	Token  string `yaml:"token" env:"TOKEN" mapstructure:"telegram_token"`
+	ChatID string `yaml:"chat_id" env:"CHAT_ID" mapstructure:"telegram_chat_id"`
 }
